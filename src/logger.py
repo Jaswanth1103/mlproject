782fc1b17d4b326ab62b41fa59cb8f34c633b10c
Jaswanth1103 <jaswanthreddy1120@gmail.com>
2025-07-28 16:57:00 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,40 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=F"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
+
+
+# import logging
+# import os
+# from datetime import datetime
+
+# # Create unique log file name
+# LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+
+# # Path where log file will be saved
+# logs_path = os.path.join(os.getcwd(), "logs")
+# os.makedirs(logs_path, exist_ok=True)
+
+# LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# # Corrected argument: filename= not file=
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,
+#     format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+#     level=logging.INFO,
+# )
+
+# if __name__ == "__main__":
+#     logging.info("Logging has started")
+
